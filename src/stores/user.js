f62f9533b575a47891e18449605da059861ca8f4
kaiesmahmud <kaiesmahmud@gmail.com>
2023-10-18 02:06:28 +0600

    demo commented

---
+++ src/stores/user.js
@@ -0,0 +1,12 @@
+//Local Storage Settings
+
+
+// import { writable } from 'svelte/store';
+
+// export const user = writable(null);
+
+// user.subscribe((value) => {
+//     localStorage.setItem('user', JSON.stringify(value));
+//   });
+  
+//   user.set(JSON.parse(localStorage.getItem('user')));
