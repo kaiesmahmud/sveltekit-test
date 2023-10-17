a5842dba3dda5673b11f3fe0b5e42f53014c868a
kaiesmahmud <kaiesmahmud@gmail.com>
2023-10-18 02:05:57 +0600

    teacher dashboard updated

--- src/routes/teacher/dashboard/+page.svelte
+++ src/routes/teacher/dashboard/+page.svelte
@@ -1,1 +1,6 @@
-<h1>teacher-dashboard</h1>
+<div class="flex justify-between">
+    <h1>Teacher-dashboard</h1>
+    <form  method="post">
+        <button class="p-3 border bg-slate-500 rounded font-semibold text-white" >Logout</button>
+    </form>
+</div>
