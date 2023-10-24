c5bcb18dd725719ad1c1cc1d666b944e6d77dda7
kaiesmahmud <kaiesmahmud@gmail.com>
2023-10-24 18:36:28 +0600

    code optimized

--- src/routes/teacher/dashboard/[courseid]/edit/+page.svelte
+++ src/routes/teacher/dashboard/[courseid]/edit/+page.svelte
@@ -1,15 +1,14 @@
 <script>
-    import SectionDetails from '$lib/components/SectionDetails.svelte';
     import CourseNameUpdate from '$lib/components/CourseNameUpdate.svelte';
     import ImageUpdate from '$lib/components/ImageUpdate.svelte';
-    import Summary from '$lib/components/Summary.svelte';
-  import UpdateSummary from '../../../../../lib/components/UpdateSummary.svelte';
-  import UpdateSectionDetails from '../../../../../lib/components/UpdateSectionDetails.svelte';
+    import UpdateSummary from '$lib/components/UpdateSummary.svelte';
+    import UpdateSectionDetails from '$lib/components/UpdateSectionDetails.svelte';
 
     export let data
     let viewCourseDetails = data.viewCourseDetails
+
     console.log("Got ",viewCourseDetails)
-    const {shortname,fullname,courseimage,courseSectionDetails,summary} = viewCourseDetails
+    const {shortname,fullname,courseimage,courseSectionDetails,summary,id} = viewCourseDetails
 
     let cssbtn = "px-5 py-3 font-semibold text-xl bg-slate-800 text-white/80 hover:text-white hover:bg-slate-700 rounded transition-all ease-in duration-150"
     let inputCSS = "border border-black p-5 rounded w-full text-2xl"
@@ -22,7 +21,7 @@
 </div>
 
 <div class="custom__container mt-10">
-    <CourseNameUpdate shortname={shortname} fullname={fullname} inputCSS={inputCSS} cssbtn={cssbtn}/>
+    <CourseNameUpdate courseid={id} token={data.user?.token} shortname={shortname} fullname={fullname} inputCSS={inputCSS} cssbtn={cssbtn}/>
 </div>
 {#if summary}
     <UpdateSummary summary={summary} cssbtn={cssbtn} />
@@ -30,7 +29,6 @@
 
 <div class="mt-10 flex flex-col gap-10 ">
     {#each courseSectionDetails as section}
-        <!-- <SectionDetails section={section}/> -->
         <UpdateSectionDetails section={section}/>
     {/each}
 </div>
